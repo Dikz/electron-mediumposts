91cc2843a0ec6194e23b0f2eb4557d491afc0a77
Dikz <cicerogabriel12@hotmail.com>
2018-12-20 04:57:27 -0300

    Def. form de pesquisa e div de resultados com SearchController.formHandleSearch

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 const NavbarController = require('./src/Controllers/NavbarController');
+const SearchController = require('./src/Controllers/SearchController');
 
 $(document).ready(() => {
   NavbarController.basic();
+  SearchController.formHandleSearch('searchForm', 'searchResults');
 });
